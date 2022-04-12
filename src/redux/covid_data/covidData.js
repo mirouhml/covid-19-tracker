487e7fea2567bd1fd53d8cf2b1f57d43ef53ce50
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-12 14:57:41 +0100

    Use fetch service to create a redux reducer.

---
+++ src/redux/covid_data/covidData.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from 'uuid';
+import FetchCovidData from '../../services/FetchCovidData';
+
+const GET_COVID_DATA = 'GET_COVID_DATA';
+
+const getCovidData = () => async (dispatch) => {
+  try {
+    const res = await FetchCovidData();
+    const dataArray = Object.entries(res.data);
+    const covidData = [];
+    dataArray.forEach((data) => {
+      if (data[1].All.country && data[1].All.continent && data[1].All.abbreviation) {
+        covidData.push({
+          id: uuidv4(),
+          confirmed: data[1].All.confirmed,
+          recovered: data[1].All.recovered,
+          deaths: data[1].All.deaths,
+          country: data[1].All.country,
+          continent: data[1].All.continent,
+          abbreviation: data[1].All.abbreviation,
+          updated: data[1].All.updated,
+        });
+      }
+    });
+    console.log(covidData);
+    dispatch({
+      type: GET_COVID_DATA,
+      covidData,
+    });
+    return Promise.resolve(covidData);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+const reducer = (covidData = [], action) => {
+  switch (action.type) {
+    case GET_COVID_DATA:
+      return action.covidData;
+    default:
+      return covidData;
+  }
+};
+
+export { getCovidData };
+export default reducer;
