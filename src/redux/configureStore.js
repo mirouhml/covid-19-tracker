0b47138202e2dc134f46d85d93afcd2462c3f818
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-13 23:37:46 +0100

    Add second reducer and combine reducers.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import reducer from './covid_data/covidData';
+import dataReducer from './covid_data/covidData';
+import historyReducer from './covid_data/covidHistory';
 
 const rootReducer = combineReducers({
-  covidData: reducer,
+  covidData: dataReducer,
+  covidHistory: historyReducer,
 });
 const initialState = {};
 const middleware = [thunk];
