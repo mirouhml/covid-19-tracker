62c9309fd05850de1188ef0f76c54e9450c1330f
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-13 23:39:57 +0100

    Add header tab in Navigation.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -51,6 +51,15 @@ const Navigation = () => {
   return (
     <div className="main">
       <div className="header">
+        <div className="details-header">
+          <button
+            type="button"
+            className="back-arrow"
+          >
+            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 320 512" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z" /></svg>
+          </button>
+          <h2 className="details-title">Covid-19 Tracker</h2>
+        </div>
         <h2>Welcome to my Covid-19 Cases tracker</h2>
         <h3>Filters:</h3>
         <div className="filters">
