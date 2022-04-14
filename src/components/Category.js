433734a4d8b149f73b42d300b7984660b2e19955
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-14 01:59:43 +0100

    Add maps to the grids.

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,15 +1,22 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
 import {
   NavLink,
 } from 'react-router-dom';
 import '../styles/Category.css';
 
 const Category = (props) => {
-  const { category } = props;
+  const { category, index } = props;
   const hide = () => {
     document.querySelector('.header').style.display = 'none';
   };
+
+  useEffect(() => {
+    const BB = document.querySelectorAll('.mapG')[index].getBBox();
+    document.querySelectorAll('.map')[index].setAttributeNS(null, 'viewBox', `${BB.x} ${BB.y} ${BB.width} ${BB.height}`);
+  }, []);
+
   return (
     <div>
       <NavLink
@@ -21,6 +28,11 @@ const Category = (props) => {
         onClick={hide}
       >
         <div className="category">
+          <svg className="map" height="7.5rem" width="8rem">
+            <g className="mapG">
+              <path fill="#2D4573" d={category.map} />
+            </g>
+          </svg>
           <h3>{category.country}</h3>
           <h3>{category.confirmed}</h3>
         </div>
